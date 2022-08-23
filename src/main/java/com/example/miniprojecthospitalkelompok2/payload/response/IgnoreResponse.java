7e7c9d39b508380dcad939dd9668557a840db118
NuzulFithra <nuzulfithraryaldi@gmail.com>
2022-08-23 12:04:19 +0700

    push again

--- src/main/java/com/example/miniprojecthospitalkelompok2/payload/response/IgnoreResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.miniprojecthospitalkelompok2.payload.response;
-
-import com.example.miniprojecthospitalkelompok2.entity.Users;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-public class IgnoreResponse {
-    @JsonIgnoreProperties("password")
-    public class GetUsers extends Users {}
-}
